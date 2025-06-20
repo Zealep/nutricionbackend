3bfbb1b1f0eba6865f9db1869b19bc159852fe27
Cristhian Pelaez <cristhianpelaez13@gmail.com>
2025-06-19 20:13:11 -0500

    delete column activo

--- src/main/java/com/zealepsoluciones/nutricionbackend/repository/MenuRepository.java
+++ src/main/java/com/zealepsoluciones/nutricionbackend/repository/MenuRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface MenuRepository extends CrudRepository<Menu, Long> {
-    List<Menu> findByActivo(Boolean activo);
 }
